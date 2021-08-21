a0f1b169d7d7217079cb481d12f669016f4bb4e8
Srujan12-hi <85944675+Srujan12-hi@users.noreply.github.com>
2021-08-21 15:52:10 +0530

    Update messages.py

--- root/messages.py
+++ root/messages.py
@@ -1,9 +1,9 @@
 class Translation(object):
-    START_TEXT = "**I'm a Rename and Convert Bot\n\nJust send me any media to change file name.\n\nUse /help command for more details\n\nMade with love by @sruja_12 **"
+    START_TEXT = "**I'm a Rename + Convert Bot\n\nJust send me any media to change file name.\n\nUse /help command for more details\n\nMade with love by @sruja_12 **"
 ######################
-    HELP_USER = """**>>Send any File/Video\n>>Select desired Option\n>>And wait for it to process files**"""
-    DOWNLOAD_MSG = "**Downloading **⏬"
-    DOWNLOAD_FAIL_MSG = "**Sorry Unkil Failed to Download File**❎"
-    UPLOAD_MSG = "**Uploading** ⏫"
-    UPLOAD_FAIL_MSG = "**Failed to Upload File**❎"
+    HELP_USER = """**>>Send any File/Video\n>>Select desired Option\n>>And wait for me to process files**"""
+    DOWNLOAD_MSG = "**Downloading it please wait**⏬"
+    DOWNLOAD_FAIL_MSG = "**Sorry Unkil Failed to Download File\nThere may be a problem with your file**❎"
+    UPLOAD_MSG = "**Uploading it to telegram please wait ** ⏫"
+    UPLOAD_FAIL_MSG = "**Failed to Upload File\nThere may be a problem with your file**❎"
     UPLOAD_DONE_MSG = "**Uploaded Successfully 💡"
